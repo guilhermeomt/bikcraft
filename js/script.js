ff15365329ff4d9639dfa08fb6e85b72b2270f23
Guilherme Tavares <guilhermet213@gmail.com>
2021-01-22 12:34:54 -0300

    Add fade-in animations to the website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-if (window.SimpleSlide) {
-
+if(window.SimpleSlide) {
 	new SimpleSlide({
 		slide: "quote",
 		time: 5000
@@ -10,5 +9,8 @@ if (window.SimpleSlide) {
 		time: 5000,
 		nav: true
 	});
+}
 
+if(window.SimpleAnime) {
+  new SimpleAnime();
 }
